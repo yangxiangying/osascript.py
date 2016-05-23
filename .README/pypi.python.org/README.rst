46596eb7845b3e182c784a8c4c7b2fb7490b0ec8
russianidiot <russianidiot.github@gmail.com>
2016-05-23 18:44:11 +0300

    ^.README/pypi.python.org/README.rst,.coveragerc,.setup/.classifiers.py,.set ...

--- .README/pypi.python.org/README.rst
+++ .README/pypi.python.org/README.rst
@@ -77,7 +77,7 @@ Feedback |github_issues| |gitter| |github_follow|
 
 `README.rst`_  - generated with `readmemako.py`_ (python+ `mako`_ templates) and `.README`_ dotfiles
 
-.. _README.rst: https://github.com/russianidiot/osascript.py/blob/master/.README/pypi.python.org/README.rst
+.. _README.rst: https://github.com/russianidiot/osascript.py/blob/master/README.rst
 .. _readmemako.py: http://github.com/russianidiot/readmemako.py/
 .. _mako: http://www.makotemplates.org/
 .. _.README: https://github.com/russianidiot-dotfiles/.README
--- py_modules/osascript.py
+++ py_modules/osascript.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from os.path import *
-from subprocess import *
-from fullpath import fullpath
+import os
 import only
 from public import public
 from rm import rm
 from subopen import subopen
 from temp import tempfile
-from tolist import tolist
 from write import write
 
 
@@ -21,7 +18,7 @@ def osascript(applescript, flags=None):
     if applescript and isinstance(applescript, list):
         applescript = "\n".join(applescript)
     temp = programfile = None
-    if applescript and exists(applescript) or exists(applescript):
+    if applescript and os.path.exists(applescript):
         programfile = applescript
     else:
         temp = programfile = tempfile()
